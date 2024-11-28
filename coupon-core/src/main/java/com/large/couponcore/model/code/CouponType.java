8f627ed6835dc5fd77cd8726d90f6f1ef3adaaca
joyj10 <micool1030@gmail.com>
2024-11-28 17:38:48 +0900

    [feat] 엔티티 설계

---
+++ coupon-core/src/main/java/com/large/couponcore/model/code/CouponType.java
@@ -0,0 +1,5 @@
+package com.large.couponcore.model.code;
+
+public enum CouponType {
+    FIRST_COME_FIRST_SERVED // 선착순 쿠폰
+}
---
+++ coupon-core/src/main/java/com/large/couponcore/model/entity/CouponIssue.java
@@ -0,0 +1,35 @@
+package com.large.couponcore.model.entity;
+
+import com.large.couponcore.model.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "coupons_issues")
+public class CouponIssue extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long couponId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime dataIssued;
+
+    private LocalDateTime dataUsed;
+}
