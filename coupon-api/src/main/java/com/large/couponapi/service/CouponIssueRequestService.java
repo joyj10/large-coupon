02ff3ad81e648e37b6f2712de05db44284762ae1
joyj10 <micool1030@gmail.com>
2024-11-28 23:47:52 +0900

    [feat] LOCK 적용 방법 : redis lock

--- coupon-api/src/main/java/com/large/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/large/couponapi/service/CouponIssueRequestService.java
@@ -1,6 +1,7 @@
 package com.large.couponapi.service;
 
 import com.large.couponapi.controller.dto.CouponIssueRequestDto;
+import com.large.couponcore.component.DistributeLockExecutor;
 import com.large.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,11 +12,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CouponIssueRequestService {
     private final CouponIssueService couponIssueService;
-
+    private final DistributeLockExecutor distributeLockExecutor;
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        synchronized (this) {
-            couponIssueService.issue(requestDto.couponId(), requestDto.userId());
-        }
+        distributeLockExecutor.execute(
+                getLockName(requestDto.couponId()),
+                10000,
+                10000,
+                () -> couponIssueService.issue(requestDto.couponId(), requestDto.userId())
+        );
         log.info("쿠폰 발급 완료. couponI:d %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
+    }
+
+    private String getLockName(Long couponId) {
+        return "lock_" + couponId;
     }
 }
--- coupon-core/build.gradle.kts
+++ coupon-core/build.gradle.kts
@@ -9,6 +9,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation("org.redisson:redisson-spring-boot-starter:3.16.4")
 }
 
 tasks.withType<Test> {
